bb1e99d993ec207f8704df5b0265668acdd9cfc9
Tomasz Wlislocki <twlislocki@parisengineers.pl>
2015-11-26 02:36:35 +0100

    Assets fix

---
+++ public/assets/aleph-angular-8355673f7baa1a83aaf10674fc0fb39b.js
@@ -0,0 +1,12 @@
+(function() {
+  this.app = angular.module('alephBank', []);
+
+  app.controller('newTransferController', function($scope) {
+    return $scope.data = gon.x;
+  });
+
+  app.controller('historyController', function($scope) {});
+
+  app.controller('helpController', function($scope) {});
+
+}).call(this);

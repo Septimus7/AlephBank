cbca3d9a062395acbe29854f2184a8f829b66edc
dmirecki <dmirecki.dm@gmail.com>
2015-11-26 17:10:45 +0100

    Damian user added. Colon after login added.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,13 @@ users = User.first_or_create([
                                      login: 'twlislocki',
                                      password: 'MareczekJestSpoko',
                                      password_confirmation: 'MareczekJestSpoko'
+                                 },
+                                 {
+                                     first_name: 'Damian',
+                                     last_name: 'Mirecki',
+                                     login: 'dmirecki',
+                                     password: 'raz',
+                                     password_confirmation: 'raz'
                                  }
                              ])
 # Damian albo tu sobie możesz stworzyć konto albo przez konsolę - na heroku robisz `heroku run rails c production`

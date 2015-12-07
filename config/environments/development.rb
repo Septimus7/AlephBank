73cd6097379742aaef0f8fb25235bfe4d618e113
Tomasz Wlislocki <twlislocki@parisengineers.pl>
2015-12-07 21:29:03 +0100

    Heroku fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,6 +38,8 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.web_console.whitelisted_ips = '10.0.2.2'
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
